f2e9b83dcd52635b23a2c4d56c5c56e3327cc3a0
alipaoli <55110718+alipaoli@users.noreply.github.com>
2019-12-05 16:14:10 -0400

    Add files via upload

---
+++ Nomina-Draft-1.pro
@@ -0,0 +1,10 @@
+TEMPLATE = app
+CONFIG += console c++11
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += \
+        --salary--.cpp \
+        a-template.cpp \
+        c-functions.cpp \
+        z-main.cpp
---
+++ z-main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+const string nick = "buddy";
+
+int cool(string nn)
+{
+    nn = nick;
+    cout << "That's neat, " << nn << "!" << endl;
+    return 0;
+}
+/*
+vector<Empleado> empleados;
+vector<NomData> nomina;
+empleados = Leer(intro()); //Recibe el archivo y lo lee
+*/
